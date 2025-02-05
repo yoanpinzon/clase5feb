ffa85d5248d264402a8963e937a21647982a11e7
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-05 08:10:15 -0500

    main protozoario

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+
     system("chcp 65001 >nul");
-    vector<int> numeros = {1, 2, 3, 4, 5};
-    numeros.push_back(6); // Agregar un número al final
 
-    cout << "Elementos del vector: ";
-    for (int num : numeros) {
-        cout << num << " ";
+    if (argc < 3) { // Se esperan al menos 2 argumentos adicionales
+        cerr << "Error: Debes proporcionar al menos dos argumentos.\n";
+        cerr << "Uso: ./" << argv[0] << " <nombre> <edad> <ciudad> <país>\n";
+        return 1; // Código de error
     }
-    cout << endl;
 
-    cout << "El tamaño del vector es: " << numeros.size() << endl;
-    return 0;
+    cout << "Nombre del programa: " << argv[0] << endl;
+    cout << "Argumentos recibidos:\n";
+
+    cout << "  Nombre: " << argv[1] << endl;
+    cout << "  Edad: " << argv[2] << " años" << endl;
+
+    if (argc >= 4) cout << "  Ciudad: " << argv[3] << endl;
+    if (argc >= 5) cout << "  País: " << argv[4] << endl;
+
+    cout << "Ejecución finalizada correctamente.\n";
+    return 0; // Éxito
 }
