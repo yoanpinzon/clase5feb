2b9377a0cf25477b02b1128cb4cbebf18315a662
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-05 08:59:52 -0500

    & &&

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-    char a = 200;
-    cout << a << " " << (int)a << endl;
+    int david = 123;
+    int h = 222;
+    int resultado;
+
+    resultado = david & h;
+
+    cout << "resultado=" << resultado << endl;
+
     return 0;
 }
