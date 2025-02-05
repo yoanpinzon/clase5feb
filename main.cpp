2ba911dcb4565d6e5d658c0e56f9f0a5b4414230
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-05 08:18:58 -0500

    entendiendo cerr

--- main.cpp
+++ main.cpp
@@ -1,25 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main() {
+    cout << "Maria\n";
+    cout << "fue al parque\n";
+    cerr << "Mundo cruel\n";
+    cout << "Mundo mas cruel\n";
+    cout << "y fue feliz con hanna\n";
 
-    system("chcp 65001 >nul");
-
-    if (argc < 3) { // Se esperan al menos 2 argumentos adicionales
-        cerr << "Error: Debes proporcionar al menos dos argumentos.\n";
-        cerr << "Uso: ./" << argv[0] << " <nombre> <edad> <ciudad> <país>\n";
-        return 1; // Código de error
-    }
-
-    cout << "Nombre del programa: " << argv[0] << endl;
-    cout << "Argumentos recibidos:\n";
-
-    cout << "  Nombre: " << argv[1] << endl;
-    cout << "  Edad: " << argv[2] << " años" << endl;
-
-    if (argc >= 4) cout << "  Ciudad: " << argv[3] << endl;
-    if (argc >= 5) cout << "  País: " << argv[4] << endl;
-
-    cout << "Ejecución finalizada correctamente.\n";
-    return 0; // Éxito
+    return 0;
 }
