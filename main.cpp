f3d10f9b27d9597da5fcd65c043789f1c5b70817
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-05 07:56:05 -0500

    ejemplo de string

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
 
 int main() {
-
-    system("chcp 65001 >nul");
-
-    double base = 2.0, exponente = 4.0;
-    cout << "2^4 = " << pow(base, exponente) << endl;
-    cout << "Raíz cuadrada de 25: " << sqrt(25) << endl;
-    cout << "Seno de 90 grados: " << sin(90 * M_PI / 180) << endl;
+    string nombre = "Juan";
+    string saludo = "Hola, " + nombre + "!";
+    cout << saludo << endl;
+    cout << "El saludo tiene " << saludo.length() << " caracteres." << endl;
     return 0;
 }
