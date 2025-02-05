fd8effb97cccfd5f3da595c9c022649b3856b23e
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-05 08:33:48 -0500

    plop --kill all

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    cout << "Maria\n";
-    cout << "fue al parque\n";
-    cerr << "Mundo cruel\n";
-    cout << "Mundo mas cruel\n";
-    cout << "y fue feliz con hanna\n";
-
+    char a = 200;
+    cout << a << " " << (int)a << endl;
     return 0;
-
-//    515  ./clase5feb.exe
-//    516  ./clase5feb.exe 2> popo.txt 1>koko.txt
-
-
 }
