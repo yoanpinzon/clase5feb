21aaaff8388f8c2cef65c607b743b18bf6175c70
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-05 07:53:10 -0500

    ejemplo de cmath

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main() {
-    cout << "Hello, World" << endl;
+
+    system("chcp 65001 >nul");
+
+    double base = 2.0, exponente = 4.0;
+    cout << "2^4 = " << pow(base, exponente) << endl;
+    cout << "Raíz cuadrada de 25: " << sqrt(25) << endl;
+    cout << "Seno de 90 grados: " << sin(90 * M_PI / 180) << endl;
     return 0;
 }
